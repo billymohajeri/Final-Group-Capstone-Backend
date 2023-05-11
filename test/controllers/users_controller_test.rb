97d93136520b438c4f0e834d1642ff75f9b18c0a
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-05-11 16:02:08 +0300

    Add room migration, controller and model

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,35 +1,35 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get users_url, as: :json
     assert_response :success
   end
 
-  test "should create user" do
-    assert_difference("User.count") do
+  test 'should create user' do
+    assert_difference('User.count') do
       post users_url, params: { user: { username: @user.username } }, as: :json
     end
 
     assert_response :created
   end
 
-  test "should show user" do
+  test 'should show user' do
     get user_url(@user), as: :json
     assert_response :success
   end
 
-  test "should update user" do
+  test 'should update user' do
     patch user_url(@user), params: { user: { username: @user.username } }, as: :json
     assert_response :success
   end
 
-  test "should destroy user" do
-    assert_difference("User.count", -1) do
+  test 'should destroy user' do
+    assert_difference('User.count', -1) do
       delete user_url(@user), as: :json
     end
 
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+class RoomTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
