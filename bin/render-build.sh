01feb0e0cbda4b6295e32d4b1d1487af001943a8
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-05-25 17:33:59 +0300

    Add settings to deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,11 @@
 set -o errexit
 
 bundle install
+# clean
+rm -rf public
+# build
+npm install --prefix client && npm run build --prefix client
+# migrate
 bundle exec rake db:migrate
+# postbuild
+cp -a client/build/. public/
